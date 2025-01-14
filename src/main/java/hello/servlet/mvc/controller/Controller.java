d5d733d4f661b78dc0549ba4a504502f9da36dd6
Kiki1875b <kevinheo0413@gmail.com>
2025-01-14 15:07:38 +0900

    springmvc example

--- src/main/java/hello/servlet/mvc/controller/Controller.java
+++
@@ -1,9 +0,0 @@
-package hello.servlet.mvc.controller;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.web.servlet.ModelAndView;
-
-public interface Controller {
-    ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception;
-}
